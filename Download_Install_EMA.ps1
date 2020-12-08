0173381bdb58f3116ef37d84a5c58e06d084e8ad
asolano2013 <adricarosol92@hotmail.com>
2020-12-08 11:59:54 -0800

    Update Download_Install_EMA.ps1

--- Download_Install_EMA.ps1
+++ Download_Install_EMA.ps1
@@ -6,6 +6,12 @@
 #                                        #
 ##########################################
 
+$host = $Args[0]
+$dbserver = $Args[1]
+$dbname = $Args[2]
+$guser = $Args[3]
+$gpass = $Args[4]
+
 # Create C:\Temp path if it doesn't exist
 
 $path = "C:\Temp"
@@ -17,12 +23,10 @@ If(!(test-path $path)){
 
 $url = "https://github.com/asolano2013/EMATemplate/raw/main/Ema_Install_Package_1.3.3.1.exe"
 $output = "C:\Temp\EMAInstall.zip"
-$start_time = Get-Date
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 
 #Extract EMA Install file
@@ -32,6 +36,6 @@ add-type -AssemblyName System.IO.Compression.FileSystem
 
 #Run EMA Installer exe
 
-$args = @("FULLINSTALL","--host=<fqdn>","--dbserver=<dbserver>","--db=<dbname>","--guser=<guser_email>","--gpass=<gpassword>","--verbose","--console","--accepteula")
+$args = @("FULLINSTALL","--host=$host","--dbserver=$dbserver","--db=$dbname","--guser=$guser","--gpass=$gpass","--verbose","--console","--accepteula")
 Start-Process -Filepath "C:\Temp\EMAInstall\EMAServerInstaller.exe" -Verb RunAs -ArgumentList $args -WorkingDirectory "C:\Temp\EMAInstall"
 
